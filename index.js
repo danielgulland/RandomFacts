e10e64620cd48b061cdcef01e7871bdbee8e8dfa
danielgulland <dannygulland@gmail.com>
2024-08-25 19:24:15 -0700

    test

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
 });
 
 const PORT = process.env.PORT || 3000;
