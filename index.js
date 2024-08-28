6dc97b045281133d5d9c606731e27c3095d0bb15
unknown <nathanjsmith95@gmail.com>
2024-08-27 22:20:57 -0700

    added database class and get user

--- index.js
+++ index.js
@@ -1,58 +1,12 @@
 const express = require("express");
 const app = express();
-const sqlite3 = require("sqlite3").verbose();
-const path = require("path");
+const Database = require("./Database.js");
 
-let dbPath = path.resolve(__dirname, "mydb.sqlite");
-
-// Open a database connection
-let db = new sqlite3.Database(dbPath, (err) => {
-  if (err) {
-    console.error("Error opening database:", err.message);
-    return;
-  }
-  console.log("Connected to the SQLite database.");
-
-  // Create a table if it doesn't exist
-  db.run(
-    `CREATE TABLE IF NOT EXISTS user (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      name TEXT NOT NULL,
-      email TEXT UNIQUE NOT NULL
-    )`,
-    (err) => {
-      if (err) {
-        console.error("Error creating table:", err.message);
-        return;
-      }
-      console.log("Table created or already exists.");
-
-      // Insert data after the table has been created or confirmed to exist
-      db.run(
-        `INSERT INTO user (name, email) VALUES (?, ?)`,
-        ["Dan", "dan@yahoo.com"],
-        function (err) {
-          if (err) {
-            console.error("Error inserting data:", err.message);
-            return;
-          }
-          console.log(
-            `A new user has been inserted with rowid ${this.lastID}.`
-          );
-
-          // Close the database connection after the insert
-          db.close((err) => {
-            if (err) {
-              console.error("Error closing database:", err.message);
-            } else {
-              console.log("Database connection closed.");
-            }
-          });
-        }
-      );
-    }
-  );
-});
+let db = new Database("./mydb.sqlite");
+db.createTable();
+db.insertUser("nate", 30);
+db.getUsers();
+db.close();
 
 app.get("/", (req, res) => {
   res.send("Hello, world!!");
